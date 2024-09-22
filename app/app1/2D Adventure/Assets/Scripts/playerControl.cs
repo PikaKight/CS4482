958afdcc82359b6bd9d00c17e1fbb5768d2aa4cf
PikaKight <tmmarcus73@gmail.com>
2024-09-22 12:57:52 -0400

    updating app1

--- app/app1/2D Adventure/Assets/Scripts/playerControl.cs
+++ app/app1/2D Adventure/Assets/Scripts/playerControl.cs
@@ -9,20 +9,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     public float speed;
-    public Vector2 jumpHeight;
 
     public InputAction moveAction;
-    public InputAction jumpAction;
-
-    public Rigidbody2D rb;
-
-    private bool onGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
         moveAction.Enable();
-        jumpAction.Enable();
     }
 
     // Update is called once per frame
@@ -32,12 +25,5 @@ public class PlayerController : MonoBehaviour
         Vector2 position = (Vector2)transform.position + (move * speed * Time.deltaTime);
 
         transform.position = position;
-
-        if (jumpAction.IsPressed() && onGround)
-        {
-            rb.AddForce(jumpHeight, ForceMode2D.Impulse);
-            onGround = false;
-        }
-
   }
 }
